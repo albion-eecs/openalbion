d6c1532b39fbf8b05a92ba2552de1c8ec0a44202
Alexander <201009224+diocletiana@users.noreply.github.com.>
2025-06-16 20:11:30 -0500

    refactor: delete previous docs implementation

--- docs/src/app/api-endpoints/_meta.tsx
+++
@@ -1,9 +0,0 @@
-const meta = {
-    'class-sizes': 'Class Sizes',
-    departments: 'Departments',
-    enrollment: 'Enrollment',
-    faculty: 'Faculty',
-    headcounts: 'Headcounts' 
-}
-
-export default meta;
