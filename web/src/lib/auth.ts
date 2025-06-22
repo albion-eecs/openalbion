ccb53fbee662f64facdd1fe8b720003deef05776
Alexander <201009224+diocletiana@users.noreply.github.com.>
2025-06-22 08:16:35 -0500

    chore: secret explicit option to better auth authentication configuration

--- web/src/lib/auth.ts
+++ web/src/lib/auth.ts
@@ -4,6 +4,7 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../../db/index";
 
 export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET,
   database: drizzleAdapter(db, {
     provider: "sqlite",
   }),
