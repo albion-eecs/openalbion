9909d12b5f53792af6c38ea8439242aa1a5eb685
Justin <201009224+diocletiana@users.noreply.github.com>
2026-01-22 22:38:50 -0500

    ref: temp

---
+++ ref/apps/web/next.config.ts
@@ -0,0 +1,14 @@
+import "@ref/env/web";
+import type { NextConfig } from "next";
+
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+  transpilePackages: ["shiki"],
+};
+
+export default nextConfig;
+
+initOpenNextCloudflareForDev();
